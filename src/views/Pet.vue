faeb482ef25b6f8a158806898b47126691d8a969
ali <aliasanov07@gmail.com>
2019-07-19 11:17:58 +0300

    added store

--- src/views/Pet.vue
+++ src/views/Pet.vue
@@ -1,5 +1,28 @@
 <template>
   <div>
-    Pet
+    <h1>{{ pet.name }} ({{ $route.params.species }})</h1>
+    <p>Age: {{ pet.age }} years old.</p>
+    <p>Breed: {{ pet.breed }}</p>
   </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  data () {
+    return {
+      pet: {}
+    }
+  },
+  computed: {
+    ...mapState([
+      'cats',
+      'dogs'
+    ])
+  },
+  mounted () {
+    this.pet = this[this.$route.params.species][this.$route.params.id]
+  }
+}
+</script>
